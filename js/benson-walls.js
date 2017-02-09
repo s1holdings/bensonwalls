3252e467c7107a237fa090a09748036833ce590f
joealongifx <contact@joeyalongifx.com>
2017-02-09 04:50:21 -0500

    Functioning Update - Mobile Fixes
    Toggle Menu - Update for Mobile
    Wallcovering Cards - Update for Mobile
    Bootstrap - v3.3.7
    Scroll Ease - Dependency (jQuery Easing v1.3)
    Custom JS for Scroll

---
+++ js/benson-walls.js
@@ -0,0 +1,33 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    /***************** Navbar-Collapse ******************/
+
+    $(window).scroll(function () {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+    });
+
+    /***************** Page Scroll ******************/
+
+    $(function () {
+        $('a.page-scroll').bind('click', function (event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
+
+    /***************** Scroll Spy ******************/
+
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    })
+
+})(jQuery); // End of use strict
